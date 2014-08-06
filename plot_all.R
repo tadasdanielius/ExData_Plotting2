414c4d1b9523a38323312f2e8d21b73d1675f7a7
Tadas <tadas.danielius@gmail.com>
2014-08-06 12:54:31 +0100

    removed 'global env clean' before each plot in plot_all.R

--- plot_all.R
+++ plot_all.R
@@ -4,22 +4,17 @@ rm(list = ls(all = TRUE));
 message("Plot 1");
 source('~/plot1.R');
 
-rm(list = ls(all = TRUE));
 message("Plot 2");
 source('~/plot2.R');
 
-rm(list = ls(all = TRUE));
 message("Plot 3");
 source('~/plot3.R');
 
-rm(list = ls(all = TRUE));
 message("Plot 4");
 source('~/plot4.R');
 
-rm(list = ls(all = TRUE));
 message("Plot 5");
 source('~/plot5.R');
 
-rm(list = ls(all = TRUE));
 message("Plot 6");
 source('~/plot6.R');
